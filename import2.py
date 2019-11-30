592e343e8ad79dc1552fbc94f6380b18630b535c
StellaShi <stella@apples-MacBook-Pro.local>
2019-11-30 16:28:02 +0200

    add Custom random number range

--- import2.py
+++ import2.py
@@ -1,16 +1,21 @@
 import random
-r = random.randint (1,100)
+start = input('Please decide the random number range（starting）:')
+end = input('Please decide the random number range（endging）:')
+start = int(start)
+end = int(end)
+
+r = random.randint (start,end)
 count = 0
 while True:
 	count += 1 # count = count + 1
-	p = input ('enter number please:')
-	p = int(p)
-	if p == r:
+	num = input ('enter number please:')
+	num = int(num)
+	if num == r:
 		print ('u are right')
 		print ( 'this is the ' , count , ' times u guess ')
 		break
-	elif p > r:
-		print ( ' greater then answer ')
-	elif p < r:
-		print ( ' smaller then answer ')
+	elif num > r:
+		print ( num, ' is greater then answer ')
+	elif num < r:
+		print ( num ,' is smaller then answer ')
 	print ( 'this is the' , count , ' times u guess ') 
